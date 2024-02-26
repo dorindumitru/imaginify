a94d9261f78bbd6ef5aaf949105cf5f427a9286d
dorindumitru <dorincosmin.dumitru@yahoo.com>
2024-02-26 10:16:51 +0200

    final version

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -4,11 +4,11 @@ export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: URL;
+  secureURL: string;
   width?: number;
   height?: number;
   config?: object;
-  transfoarmationUrl?: URL;
+  transformationUrl?: string;
   aspectRatio?: string;
   color?: string;
   prompt?: string;
@@ -17,19 +17,19 @@ export interface IImage extends Document {
     firstName: string;
     lastName: string;
   };
-  createdAt: Date;
-  updatedAt: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
   transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
-  secureUrl: { type: URL, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transfoarmationUrl: { type: URL },
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
